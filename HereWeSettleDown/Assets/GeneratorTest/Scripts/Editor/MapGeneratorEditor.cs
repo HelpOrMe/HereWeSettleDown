3b2b5a60a6835c625cb027aeed2e33a69fffff92
sah-py <catmurcat.balacky@gmail.com>
2020-02-11 01:12:43 +0200

    Create simple noise generator

---
+++ HereWeSettleDown/Assets/GeneratorTest/Scripts/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Generator.Test._Editor
+{
+    [CustomEditor(typeof(MapGenerator))]
+    public class MapGeneratorEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            MapGenerator mapGenerator = (MapGenerator)target;
+            if (mapGenerator.autoUpdate)
+            {
+                mapGenerator.GenerateMap();
+            }
+            if (GUILayout.Button("Generate map"))
+            {
+                mapGenerator.GenerateMap();
+            }
+        }
+    }
+}
---
+++ HereWeSettleDown/Assets/GeneratorTest/Scripts/MapGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Generator.Test
+{
+    public class MapGenerator : MonoBehaviour
+    {
+        public MapDisplay mapDisplay;
+
+        public int mapWidth;
+        public int mapHeight;
+        public float noiseScale;
+
+        public int octaves;
+        [Range(0f, 1f)]
+        public float persistance;
+        public float lacunarity;
+
+        public int seed;
+        public Vector2 offset;
+
+        public bool autoUpdate;
+
+        public void GenerateMap()
+        {
+            // Generate new noise map and display it
+            float[,] noiseMap = Noise.GenerateNoiseMap(seed, mapWidth, mapHeight, noiseScale, octaves, persistance, lacunarity, offset);
+            mapDisplay.DrawNoiseMap(noiseMap);
+        }
+
+        void OnValidate()
+        {
+            if (mapWidth < 1) mapWidth = 1;
+            if (mapHeight < 1) mapHeight = 1;
+            if (lacunarity < 1) lacunarity = 1;
+            if (octaves < 0) octaves = 0;
+        }
+    }
+}
