790f6c6da044b25fd45f8822e309f0b5dc13aa4d
sah-py <catmurcat.balacky@gmail.com>
2020-02-14 20:25:05 +0200

    Restructure file system & chunk visible updating

---
+++ HereWeSettleDown/Assets/Scripts/Generator/Chunk.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+namespace Generator
+{
+    public class Chunk
+    {
+        public GameObject chunkObject;
+        public MeshData meshData;
+
+        public Vector2Int mapPosition;
+        public Vector2Int size;
+
+        public bool visible { get; private set; }
+
+        MeshFilter meshFilter;
+
+        public Chunk(GameObject terrain, Transform parent, bool visible, Vector2Int mapPosition, Vector2Int size, MeshData meshData)
+        {
+            this.size = size;
+            this.mapPosition = mapPosition;
+            this.meshData = meshData;
+
+            CreateChunkObject(terrain, parent);
+            DrawMesh();
+            UpdateMeshCollider();
+            SetVisible(visible);
+        }
+
+        void CreateChunkObject(GameObject terrain, Transform parent)
+        {
+            chunkObject = Object.Instantiate(terrain, parent);
+            chunkObject.name = "Chunk" + mapPosition;
+
+            Vector3 scale = chunkObject.transform.localScale;
+            chunkObject.transform.position = new Vector3(mapPosition.x * (size.x - 1) * scale.x, 0, mapPosition.y * (size.y - 1) * scale.z);
+
+            meshFilter = chunkObject.GetComponent<MeshFilter>();
+        }
+
+        void UpdateMeshCollider()
+        {
+            MeshCollider collider = chunkObject.GetComponent<MeshCollider>();
+            if (collider)
+                Object.Destroy(collider);
+            collider = chunkObject.AddComponent<MeshCollider>();
+            if (meshFilter)
+            {
+                collider.sharedMesh = meshFilter.sharedMesh;
+            }
+        }
+
+        public void DrawMesh()
+        {
+            if (meshFilter)
+                meshFilter.sharedMesh = meshData.CreateMesh();
+        }
+
+        public void SetVisible(bool state)
+        {
+            if (chunkObject)
+                chunkObject.SetActive(state);
+            visible = state;
+        }
+    }
+}
+
---
+++ HereWeSettleDown/Assets/Scripts/Generator/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Generator._Editor
+{
+    [CustomEditor(typeof(MapGenerator), true)]
+    public class MapGeneratorEditor : Editor
+    {
+        static bool autoUpdate;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            MapGenerator mapGenerator = (MapGenerator)target;
+
+            autoUpdate = EditorGUILayout.Toggle("Auto Update", autoUpdate);
+
+            if (autoUpdate || GUILayout.Button("Generate map"))
+            {
+                if (MapGenerator.chunkMap != null)
+                {
+                    for (int x = 0; x < MapGenerator.chunkMap.GetLength(0); x++)
+                    {
+                        for (int y = 0; y < MapGenerator.chunkMap.GetLength(1); y++)
+                        {
+                            if (MapGenerator.chunkMap[x, y].chunkObject)
+                                Destroy(MapGenerator.chunkMap[x, y].chunkObject);
+                        }
+                    }
+                }
+                
+                mapGenerator.GenerateMap();
+            }
+        }
+    }
+}
